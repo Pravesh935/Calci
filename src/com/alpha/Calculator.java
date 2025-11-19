267f056a94f4250654e49ec714c0b7c1b1409540
Pravesh935 <praveshkumar5502@gmail.com>
2025-11-19 15:24:20 +0530

    This is initial commit

---
+++ src/com/alpha/Calculator.java
@@ -0,0 +1,8 @@
+package com.alpha;
+
+public interface Calculator {
+   abstract public int add(int a, int b);
+   abstract public int sub(int a, int b);
+   abstract public int mul(int a, int b);
+   abstract public int div(int a, int b);
+}
