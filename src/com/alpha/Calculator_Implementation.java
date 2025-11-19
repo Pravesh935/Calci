e7e4fe4c0dc6fd6d303f232b9fa6f9354c6cf514
Pravesh935 <praveshkumar5502@gmail.com>
2025-11-19 15:41:23 +0530

    This is second commit

---
+++ src/com/alpha/Calculator_Implementation.java
@@ -0,0 +1,29 @@
+package com.alpha;
+
+public class Calculator_Implementation implements Calculator{
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
